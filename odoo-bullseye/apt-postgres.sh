5dcdfefcc77e9d87d8cb18b5638800d2c7b79c2f
Fabio Missio <fabiomix@users.noreply.github.com>
2023-11-15 17:51:30 +0100

    Drop Ubuntu, embrace Debian
    Change disk size. Install PostgreSQL, Docker and Python3 packages via bash.

---
+++ odoo-bullseye/apt-postgres.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+curl -sS https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/postgresql.gpg > /dev/null
+apt-get update
+apt-get install -y postgresql-14 postgresql-client-14
