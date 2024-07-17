789d7aaeb6a8511d93f2a3d6bcff751e3246c9a8
Fabio Missio <fabiomix@users.noreply.github.com>
2024-07-17 19:01:44 +0200

    Setup on bookworm using Ansible

--- odoo-bullseye/apt-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
-apt-get update
-apt-get install -y containerd.io docker-ce docker-ce-cli docker-compose-plugin
-usermod -aG docker vagrant
